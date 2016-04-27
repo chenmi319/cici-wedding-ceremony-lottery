9a02a3c8892c070feffcc832a5df28c0b8a4df4e
mi.chen <mi.chen@alo7.com>
2016-04-27 21:10:21 +0800

    add no duplicate result

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
 var g_Interval = 1;
-var g_PersonCount = 500;//参加抽奖人数
+var g_PersonCount = 10;//参加抽奖人数
 var g_Timer;
 var running = false;
+var historyNum = [];
+var currentNum = 0;
 function beginRndNum(trigger){
-	if(running){
-		running = false;
-		clearTimeout(g_Timer);		
-		$(trigger).val("开始");
-		$('#ResultNum').css('color','hotpink');
-	}
-	else{
-		running = true;
-		$('#ResultNum').css('color','hotpink');
-		$(trigger).val("停止");
-		beginTimer();
-	}
+  if(running){
+    historyNum.push(currentNum);
+    running = false;
+    clearTimeout(g_Timer);
+    $(trigger).val("开始");
+    $('#ResultNum').css('color','hotpink');
+  }
+  else{
+    if(historyNum.length == g_PersonCount){
+      alert("所有号码都抽完啦");
+      return;
+    }
+    running = true;
+    $('#ResultNum').css('color','hotpink');
+    $(trigger).val("停止");
+    beginTimer();
+  }
 }
 
 function updateRndNum(){
-	var num = Math.floor(Math.random()*g_PersonCount+1);
-	$('#ResultNum').html(num);
+  do {
+    currentNum = Math.floor(Math.random()*g_PersonCount+1);
+  } while(-1 != historyNum.indexOf(currentNum))
+  $('#ResultNum').html(currentNum);
 }
 
 function beginTimer(){
-	g_Timer = setTimeout(beat, g_Interval);
+  g_Timer = setTimeout(beat, g_Interval);
 }
 
 function beat() {
-	g_Timer = setTimeout(beat, g_Interval);
-	updateRndNum();
+  g_Timer = setTimeout(beat, g_Interval);
+  updateRndNum();
 }
