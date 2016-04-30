9b47a3661c64103bc63d810cf2cfe6b98cb4d089
mi.chen <mi.chen@alo7.com>
2016-04-30 11:32:17 +0800

    add firework

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var currentIndex;
 var historyNums = [];
 var g_Interval = 1;
 var g_Timer;
-var clearInterval;
+var firework_interval;
 
 function setMaxNum(trigger){
   maxNum = parseInt($("#maxNum").val());
@@ -39,14 +39,14 @@ function beginRndNum(trigger){
     $('#ResultNum').css('color','red');
     $("#history").html(historyNums.join(" "));
     $("#music")[0].play();
-    //firework_interval = setInterval(playFirework,400);
+    firework_interval = setInterval(happyNewYear_keleyi_com,400);
   }else{
     running = true;
     $('#ResultNum').css('visibility','visible');
     $('#ResultNum').css('color','hotpink');
     $(trigger).val("停止");
     beginTimer();
-    //clearInterval(firework_interval);
+    clearInterval(firework_interval);
     $("#music")[0].pause();
     $("#music")[0].currentTime = 0;
   }
@@ -78,9 +78,9 @@ function reset(){
   if(g_Timer){
     clearTimeout(g_Timer);
   }
-  //if(firework_interval){
-  //  clearInterval(firework_interval);
-  //}
+  if(firework_interval){
+    clearInterval(firework_interval);
+  }
   $("#startBtn").val("开始");
   $("#resetBtn").hide();
   $('#ResultNum').css('visibility','hidden');
@@ -88,97 +88,114 @@ function reset(){
   $("#music")[0].currentTime = 0;
 }
 
+function happyNewYear_keleyi_com() {
+  new fireworks();
+}
 
 
-var firework = function(){
-  this.size = 40;
-  this.speed = 0.1;
+
+
+var yanhua = "yanhua.keleyi.com";
+var fireworks = function () {
+  this.size = 20;
   this.rise();
 }
-firework.prototype = {
-  color:function(){
-    var c = ['0','3','6','9','c','f'];
-    var t = [c[Math.floor(Math.random()*100)%6],'0','f'];
-    t.sort(function(){return Math.random()>0.5?-1:1;});
-    return '#'+t.join('');
+fireworks.prototype = {
+  color: function () {
+    var c = ['0', '3', '6', '9', 'c', 'f'];
+    var t = [c[Math.floor(Math.random() * 100) % 6], '0', 'f'];
+    if (yanhua === 'yanhua.ke' + 'leyi.' + 'com')
+      t.sort(function () { return Math.random() > 0.5 ? -1 : 1; });
+    return '#' + t.join('');
   },
-  aheight:function(){
-    var h = document.documentElement.clientHeight;
-    return Math.abs(Math.floor(Math.random()*h-200))+201;
+  aheight: function () {
+    var h = document.documentElement.clientHeight - 250;
+    return Math.abs(Math.floor(Math.random() * h - 200)) + 201;
   },
-  firecracker:function(){
+  firecracker: function () {
     var b = document.createElement('div');
-    var w = document.body.clientWidth;
-    b.style.color = this.color();
+    var w = document.documentElement.clientWidth;
     b.style.position = 'absolute';
+    b.style.color = this.color();
     b.style.bottom = 0;
-    b.style.left = Math.floor(Math.random()*w)+1+'px';
+    b.style.left = Math.floor(Math.random() * w) + 1 + 'px';
     document.body.appendChild(b);
     return b;
   },
-  rise:function(){
+  rise: function () {
     var o = this.firecracker();
     var n = this.aheight();
-    var speed = this.speed;
+    var c = this.color;
     var e = this.expl;
     var s = this.size;
     var k = n;
-    var m = function(){
-      o.style.bottom = parseFloat(o.style.bottom)+k*speed+'px';
-      k-=k*speed;
-      if(k<2){
+    var m = function () {
+      o.style.bottom = parseFloat(o.style.bottom) + k * 0.1 + 'px';
+      k -= k * 0.1;
+      if (k < 2) {
         clearInterval(clear);
-        e(o,n,s,speed);
+        e(o, n, s, c);
       }
     }
-    o.innerHTML = '*';
-    if(parseInt(o.style.bottom)<n){
-      var clear = setInterval(m,20);
+    o.innerHTML = '.';
+    if (parseInt(o.style.bottom) < n) {
+      var clear = setInterval(m, 20);
     }
   },
-  expl:function(o,n,s,speed){
-    var R = n/3;
-    var Ri = n/6;
-    var r = 0;
-    var ri = 0;
-    for(var i=0;i<s;i++){
-      var span = document.createElement('span');
-      var p = document.createElement('p');
-      span.style.position = 'absolute';
-      span.style.left = 0;
-      span.style.top = 0;
-      span.innerHTML = '*';
-      p.style.position = 'absolute';
-      p.style.left = 0;
-      p.style.top = 0;
-      p.innerHTML = '+';
-      o.appendChild(span);
-      o.appendChild(p);
-    }
-    function spr(){
-      r += R*speed;
-      ri+= Ri*speed/2;
+  expl: function (o, n, s, c) {
+    var R = n / 3, Ri = n / 6, Rii = n / 9;
+    var r = 0, ri = 0, rii = 0;
+    if (yanhua === 'yanhua.ke' + 'leyi.' + 'com')
+      for (var i = 0; i < s; i++) {
+        var span = document.createElement('span');
+        var p = document.createElement('i');
+        var a = document.createElement('a');
+        span.style.position = 'absolute';
+        span.style.fontSize = n / 10 + 'px';
+        span.style.left = 0;
+        span.style.top = 0;
+        span.innerHTML = '*';
+        p.style.position = 'absolute';
+        p.style.left = 0;
+        p.style.top = 0;
+        p.innerHTML = '*';
+        a.style.position = 'absolute';
+        a.style.left = 0;
+        a.style.top = 0;
+        a.innerHTML = '*';
+        o.appendChild(span);
+        o.appendChild(p);
+        o.appendChild(a);
+      }
+    function spr() {
+      r += R * 0.1;
+      ri += Ri * 0.06;
+      rii += Rii * 0.06;
       sp = o.getElementsByTagName('span');
-      p = o.getElementsByTagName('p');
-      for(var i=0; i<sp.length;i++){
-        sp[i].style.left=r*Math.cos(360/s*i)+'px';
-        sp[i].style.top=r*Math.sin(360/s*i)+'px';
-        p[i].style.left=ri*Math.cos(360/s*i)+'px';
-        p[i].style.top=ri*Math.sin(360/s*i)+'px';
+      p = o.getElementsByTagName('i');
+      a = o.getElementsByTagName('a');
+      for (var i = 0; i < sp.length; i++) {
+        sp[i].style.color = c();
+        p[i].style.color = c();
+        a[i].style.color = c();
+        sp[i].style.left = r * Math.cos(360 / s * i) + 'px';
+        sp[i].style.top = r * Math.sin(360 / s * i) + 'px';
+        sp[i].style.fontSize = parseFloat(sp[i].style.fontSize) * 0.96 + 'px';
+        p[i].style.left = ri * Math.cos(360 / s * i) + 'px';
+        p[i].style.top = ri * Math.sin(360 / s * i) + 'px';
+        p[i].style.fontSize = parseFloat(sp[i].style.fontSize) * 0.96 + 'px';
+        a[i].style.left = rii * Math.cos(360 / s * i) + 'px';
+        a[i].style.top = rii * Math.sin(360 / s * i) + 'px';
+        a[i].style.fontSize = parseFloat(sp[i].style.fontSize) * 0.96 + 'px';
       }
-      R-=R*speed;
-      if(R<2){
-        clearInterval(clearI);
+      R -= R * 0.1;
+      if (R < 2) {
+        o.innerHTML = '';
         o.parentNode.removeChild(o);
+        clearInterval(clearI);
       }
-    }
-    var clearI = setInterval(spr,20);
+    }if(yanhua==='yanhua.ke'+'leyi.'+'com')
+    var clearI = setInterval(spr, 20);
   }
 }
 
-function playFirework(){
-  new firework();
-}
-
-window.onload = function(){
-}
